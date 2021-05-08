91eb94569438bafab08bfe6f7dffedc223e540eb
harrim4n <711475+harrim4n@users.noreply.github.com>
2021-05-08 17:22:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     ],
     install_requires=[
         'Django>=2.2',
-        'django-crispy-forms~=1.9.0'
+        'django-crispy-forms~=1.11.*'
     ],
     extras_require={
         "dev": [
